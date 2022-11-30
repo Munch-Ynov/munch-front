6a3b6461934373e4b2a535414fb956199b0b1b80
Romain Molina <molinaromain04@gmail.com>
2022-11-30 15:10:43 +0100

    Enum made

---
+++ src/shared/interfaces/Iuser.ts
@@ -0,0 +1,10 @@
+export interface Iuser {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+    description: string;
+    image_id: string;
+    role_id: number;
+}
