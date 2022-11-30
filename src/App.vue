c243faebb8f1f42d0b12be27a334344b98ba0e15
DimitriRomano <dimitri.romano09@gmail.com>
2022-11-30 10:18:41 +0100

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
+<!-- navbar here -->
 <template>
-
+<RouterView />
 </template>
 
 <style scoped lang="scss">
-
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 </style>
