35182a60a9f9e138cf25d2929ac2c956a37ad6fa
Romain Molina <molinaromain04@gmail.com>
2022-12-14 17:16:12 +0100

    Added navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue';
+import NavbarComponent from '@/components/NavbarComponent.vue';
+
 
 </script>
 
 <template>
     
   <main>
-    <!-- put the home component here -->
-  </main>
+
+    <NavbarComponent class=" fixed  bottom-0 left-0 right-0 " /> 
+      </main>
 </template>
 
 <style scoped lang="scss">
