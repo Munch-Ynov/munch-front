34474c64dd6442445a72418c975c853b378ba621
Francois BONNIN <francois.bonnin@ynov.com>
2023-05-10 00:17:58 +0200

    Header fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import TheHeader from "@/components/HeaderComponent.vue";
 import { useRouter } from "vue-router";
 import { useUser } from "./shared/stores";
 import NavbarComponent from "@/components/NavbarComponent.vue";
+import Header from "@/components/Header.vue";
 const userStore = useUser();
 const router = useRouter();
 userStore.fetchCurrentUser();
@@ -15,7 +15,7 @@ async function logout() {
 
 <template>
   <div class="flex flex-col app-container mb-16">
-    <TheHeader :isAuthenticated="userStore.isAuthenticated" @logout="logout" />
+    <Header :isAuthenticated="userStore.isAuthenticated" @logout="logout" />
     <router-view class="flex-auto"></router-view>
     <NavbarComponent class="fixed bottom-0 left-0 right-0" />
   </div>
