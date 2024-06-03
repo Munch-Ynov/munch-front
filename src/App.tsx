175ad2708642d0d9b27b685943e1f5bcfc290ad2
francois-bonnin <francois.bonnin@ynov.com>
2024-06-03 09:17:08 +0200

    fix: update pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -12,7 +10,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setResponseApi(data.status);
+        setResponseApi(`status : ${data.status}`);
       })
       .catch((err) => {
         setResponseApi(err);
